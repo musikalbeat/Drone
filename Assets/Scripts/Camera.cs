73cdd98ce762303aabe199ec4b53ec284d8b8b4d
Whitney Tran <wtran95@gmail.com>
2021-05-29 10:51:27 -0700

    Added TMP, CameraScript, UI Components

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+- Drone Camera
+- Fixed Camera Positions Around Arena
+- UI with buttons to control camera
+
+- Reference 2 buttons: ArenaButton, DroneButton
+- When DroneButton is pressed, opens up a panel with a selection of buttons of teams
+*/
+
+public class Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
